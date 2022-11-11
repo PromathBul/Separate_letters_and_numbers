98d2c6dace493f5791504876c5be1854c082745e
Dmitry_Tikhonov <tdmv1209@gmail.com>
2022-11-11 13:01:13 +0300

    Программа разбирающая список на два подсписка с числами и строками

---
+++ main.py
@@ -0,0 +1,9 @@
+import methods as m
+
+length = m.enter_num('Введите размер массива: ')
+print('Сгенерированный массив: ')
+lst = m.create_list(length)
+print(lst)
+
+print('Отсортированный массив: ')
+print(m.separate_by_elements(lst))
